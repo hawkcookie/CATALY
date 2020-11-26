65a2e3fe2b32db1619f0a77b2e8305abd29443c5
kojiwatari <cia11130@gmail.com>
2020-11-26 22:55:33 +0900

    delete_notification3

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -5,7 +5,7 @@ class MessagesController < ApplicationController
     if Entry.where(:user_id => current_user.id, :room_id => params[:message][:room_id]).present?
       @message = Message.create(params.require(:message).permit(:user_id, :content, :room_id).merge(:user_id => current_user.id))
       redirect_to "/rooms/#{@message.room_id}"
-      @message.create_notification_message!(current_user,@message.id)
+      # @message.create_notification_message!(current_user,@message.id)
     else
       redirect_back(fallback_location: root_path)
     end
