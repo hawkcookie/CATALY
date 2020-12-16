646cff51cc51a427df85608c4269a1c3981dac97
kojiwatari <cia11130@gmail.com>
2020-12-16 20:43:55 +0900

    @media_edit

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,7 +6,7 @@ PER = 9
   def index
     #@users = User.all
     @q = User.ransack(params[:q])
-    @users = @q.result(distinct: true).page(params[:page]).per(9).order(created_at: :desc)
+    @users = @q.result(distinct: true).page(params[:page]).per(6).order(created_at: :desc)
 
 # 登録ユーザー数の表示
     @all_users =User.all
