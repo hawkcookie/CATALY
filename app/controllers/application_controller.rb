999a83bb12d23b26e0551ce173e9f596fb366af7
kojiwatari <cia11130@gmail.com>
2020-12-29 01:18:59 +0900

    pass_send2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -26,5 +26,11 @@ before_action :authenticate_user!, only: [:/]
     end
 
 
+    before_action :set_host
 
+    def set_host
+      Rails.application.routes.default_url_options[:host] = request.host_with_port
+    end
+
+    
 end
