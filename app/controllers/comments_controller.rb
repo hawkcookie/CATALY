1f10268eb56532c0c7956fd936edf678b2aae61e
kojiwatari <cia11130@gmail.com>
2021-01-12 22:21:23 +0900

    count_users

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -8,8 +8,5 @@ PER = 6
     @q = User.ransack(params[:q])
     @users = @q.result(distinct: true).where(consalting: "はい").order(current_sign_in_at: :desc).page(params[:page]).per(6)
 
-# 登録ユーザー数の表示
-    @all_users =User.all
-
   end
 end
