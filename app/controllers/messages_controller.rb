bd0ef8b9f9370cc96576d631e874bead1cf82b09
kojiwatari <cia11130@gmail.com>
2021-01-31 15:31:40 +0900

    edit_change5

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -5,7 +5,7 @@ class MessagesController < ApplicationController
     if Entry.where(user_id: current_user.id, room_id: params[:message][:room_id]).present?
       @message = Message.create(params.require(:message).permit(:user_id, :content,
                                                                 :room_id).merge(user_id: current_user.id))
-      redirect_to "/rooms/#{@message.room_id}"
+      redirect_to "/rooms/#{@message.room_id}#final"
       @message.create_notification_message!(current_user, @message.id)
 
       @room = @message.room
