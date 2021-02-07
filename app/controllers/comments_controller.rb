3cc1efc64ae91c39db9295b0be259d50a05368d9
kojiwatari <cia11130@gmail.com>
2021-02-08 00:05:31 +0900

    design4

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,6 +4,6 @@ class CommentsController < ApplicationController
   def index
     # @users = User.all
     @q = User.ransack(params[:q])
-    @users = @q.result(distinct: true).where(consalting: 'はい').order(current_sign_in_at: :desc).page(params[:page]).per(6)
+    @users = @q.result(distinct: true).where(consalting: 'する').order(current_sign_in_at: :desc).page(params[:page]).per(6)
   end
 end
